6abff9252b1581a5e909350d2371219fc99ded73
Doug Diehnelt <doug.diehnelt@gmail.com>
2013-11-02 15:12:53 -0600

    Updated hand validator. Added specs.

--- spec/PokerSpec.js
+++ spec/PokerSpec.js
@@ -1,22 +1,51 @@
-describe("Poker Analyzer", function() {
-  
-  beforeEach(function() {
-  
-  });
+describe("Poker Analyzer", function () {
 
-  it("should be able to play a Song", function() {
-    player.play(song);
-    expect(player.currentlyPlayingSong).toEqual(song);
-
-    //demonstrates use of custom matcher
-    expect(player).toBePlaying(song);
-  });
-
-  describe("invalid hands", function() {
-    it("should throw an exception if a hand is invalid", function() {
-      expect(function() {
-        poker.analyse('Ah kk');
-      }).toThrowError("hand does not contain enough cards");
+    beforeEach(function () {
+        // nothing to setup
     });
-  });
+
+    describe("valid hands", function () {
+        it("should fail on a hand with too few cards", function () {
+            expect(poker.isHandValid('8h 9d')).toBeFalsy();
+        });
+        it("should fail on a hand with too many cards", function () {
+            expect(poker.isHandValid('8h 9d 6s 7s 8s 9s')).toBeFalsy();
+        });
+        it("should pass on a valid hand", function () {
+            expect(poker.isHandValid('8h 9d 6h 3d As')).toBeTruthy();
+        });
+
+        it("should fail on a invalid card", function () {
+            expect(poker.isHandValid('8h 9d 6h 3d 1s')).toBeFalsy();
+        });
+        it("should fail on a duplicate card", function () {
+            expect(poker.isHandValid('8h 9d 6h 3d 8h')).toBeFalsy();
+        });
+    });
+    describe("analysis of hand", function () {
+        it("should validate a pair",function(){
+           expect(poker.analyze('8h 8d 7s Kh 9s')).toEqual('One Pair');
+        });
+        it("should validate 2 pairs",function(){
+            expect(poker.analyze('8h 8d 7s Kh 9s')).toEqual('Two Pairs');
+        });
+        it("should validate 3 of a kind",function(){
+            expect(poker.analyze('8h 8d 7s Kh 9s')).toEqual('Three of a Kind');
+        });
+        it("should validate a straight",function(){
+            expect(poker.analyze('8h 8d 7s Kh 9s')).toEqual('Straight');
+        });
+        it("should validate a flush",function(){
+            expect(poker.analyze('8h 8d 7s Kh 9s')).toEqual('Flush');
+        });
+        it("should validate a full house",function(){
+            expect(poker.analyze('8h 8d 7s Kh 9s')).toEqual('Full House');
+        });
+        it("should validate four of a kind",function(){
+            expect(poker.analyze('8h 8d 7s Kh 9s')).toEqual('Four of a Kind');
+        });
+        it("should validate a straight flush",function(){
+            expect(poker.analyze('8h 8d 7s Kh 9s')).toEqual('Straight Flush');
+        });
+    });
 });
--- src/poker.js
+++ src/poker.js
@@ -1,11 +1,44 @@
-var poker = function(){
-	var analyse = function(hand){
-		console.log(hand);
-		};
+var poker = (function(){
+    var validSuits = ['h', 'd', 'c', 's'];
+    var validCards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+    var analyze = function(handStr){
+        if (!isHandValid(handStr)){
+            return "invalid hand";
+        }
+        console.log(handStr);
 
-	return {
-		analyse: analyse
-	};
-}.call(this);
+    };
+    var isHandValid = function(hand){
+        var cards = hand.split(' '),
+            allCardsValid=true,
+            seenCards =[];
+        if (cards.length != 5) return false; //too many or too few cards in the hand
+        for (var i =0; i < cards.length; i++){
+            allCardsValid &= cardValid(cards[i]); //verify all cards are valid
+            if (seenCards.indexOf(cards[i]) < 0) seenCards.push(cards[i]);
+        }
+        if (seenCards.length != cards.length) return false; //duplicate card in the hand
+        return allCardsValid;
+    };
+    var cardValid = function(card){
+        var suit = card.substr(card.length-1).toLowerCase(),
+            value = card.substr(0, card.length-1).toUpperCase();
+        return validSuits.indexOf(suit) > -1 && validCards.indexOf(value) > -1;
 
-poker.analyse('Ah As Ad Ac 2c');
+    };
+    var createHand = function(handStr){
+        var hand = [],
+            cards = handStr.split(' ');
+        for (var i =0; i < cards.length; i++){
+            hand.push({
+                suit: cards[i].substr(cards[i].length-1).toLowerCase(),
+                value: cards[i].substr(0, cards[i].length-1).toUpperCase()
+            })
+        }
+        return hand;
+    }
+    return {
+        analyze: analyze,
+        isHandValid: isHandValid
+    };
+}).call(this);
